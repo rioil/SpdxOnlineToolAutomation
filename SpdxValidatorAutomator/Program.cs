b104fd1150daf3b2fc9411408f853072a2036984
rioil <rioil.dev@gmail.com>
2022-11-28 18:43:49 +0900

    プロジェクト ファイルを追加します。

---
+++ SpdxValidatorAutomator/Program.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper;
+using SpdxOnlineToolAutomation;
+using System.Globalization;
+using System.Text.Json;
+
+Console.WriteLine("SPDX Validator Automator");
+
+// 検証対象リストのCSVファイル読みこみ
+var targetListCsv = args[0];
+var targetListFileReader = new CsvReader(new StreamReader(targetListCsv), CultureInfo.CurrentCulture);
+var targets = targetListFileReader.GetRecordsAsync<ValidationTarget>();
+
+// 自動検証事項
+using var automator = new SpdxValidatorAutomator();
+var result = await automator.ExecuteAsync(targets);
+
+// 検証結果ファイル保存
+Directory.CreateDirectory("Results");
+using var stream = File.OpenWrite($"Results/result_{DateTime.Now:yyyyMMdd-HHMMss}_{Path.GetFileName(targetListCsv)}.json");
+using var writer = new Utf8JsonWriter(stream, new JsonWriterOptions() { Indented = true });
+JsonSerializer.Serialize(writer, result);
---
+++ SpdxValidatorAutomator/SpdxValidatorAutomator.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace SpdxOnlineToolAutomation
+{
+    internal class SpdxValidatorAutomator : IDisposable
+    {
+        private readonly IWebDriver _driver;
+
+        private readonly IWebElement _formatElement;
+        private readonly IWebElement _fileElement;
+        private readonly IWebElement _validateButton;
+        private readonly IWebElement _modalDiv;
+        private readonly IWebElement _modalTitle;
+        private readonly IWebElement _modalBody;
+        private readonly IWebElement _modalCloseButton;
+
+        public SpdxValidatorAutomator()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(@"https://tools.spdx.org/app/validate/");
+
+            _formatElement = _driver.FindElement(By.Id("format"));
+            //_fileElement = driver.FindElement(By.XPath("//input[@type='file']"));
+            _fileElement = _driver.FindElement(By.Id("file"));
+            _validateButton = _driver.FindElement(By.Id("validatebutton"));
+            _modalDiv = _driver.FindElement(By.Id("myModal"));
+            _modalTitle = _driver.FindElement(By.Id("modal-title"));
+            _modalBody = _driver.FindElement(By.Id("modal-body"));
+            _modalCloseButton = _driver.FindElement(By.XPath("//button[@data-dismiss=\"modal\"]"));
+        }
+
+        public void Dispose()
+        {
+            _driver.Dispose();
+        }
+
+        public async Task<ValidationResult> ExecuteAsync(IAsyncEnumerable<ValidationTarget> validationTargets)
+        {
+            var started = DateTime.Now;
+            var fileValidationResults = new List<FileValidationResult>();
+
+            await foreach (var target in validationTargets)
+            {
+                fileValidationResults.Add(await ValidateFile(target));
+            }
+
+            return new ValidationResult(started, DateTime.Now, fileValidationResults);
+        }
+
+        private async Task<FileValidationResult> ValidateFile(ValidationTarget target)
+        {
+            _formatElement.SendKeys(target.FileFormat);
+            _fileElement.SendKeys(target.FileName);
+            _validateButton.Click();
+
+            while (!_modalDiv.Displayed)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(200));
+            }
+            var result = new FileValidationResult(target, _modalTitle.Text == "Success!", _modalTitle.Text, _modalBody.Text);
+
+            _modalCloseButton.Click();
+
+            return result;
+        }
+    }
+
+    public record ValidationTarget(string FileFormat, string FileName);
+
+    public record FileValidationResult(ValidationTarget Target, bool IsValid, string Title, string Description);
+
+    public record ValidationResult(DateTime Started, DateTime Ended, List<FileValidationResult> FileValidationResults);
+}
